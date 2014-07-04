a96c8f83debb52ece1da303757ff1d6409f70962
Thomas Rueckstiess <thomas@rueckstiess.net>
2014-07-03 23:40:30 -0400

    made node async wrapper for schema(). Fixes #1.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib/mongodb-schema.js');
+var schema_sync = require('./lib/mongodb-schema.js');
+
+// async wrapper for mongodb-schema 
+var schema = function(documents, options, callback) {
+    
+    // only execute on next event loop iteration
+    process.nextTick(function () {
+        try {
+            var res = schema_sync(documents, options);
+            callback(null, res);
+        } catch (e) {
+            callback(e);
+        }
+    }); 
+}
+
+module.exports = schema;

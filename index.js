7a5a32d46bd59c966c71364b4544d9ba64f79725
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-04-27 12:11:01 +1000

    whitespace

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 var schema_sync = require('./lib/mongodb-schema.js');
 
-// async wrapper for mongodb-schema 
+// async wrapper for mongodb-schema
 var schema = function(documents, options, callback) {
-    
-    if (typeof options === 'function') {
-        callback = options;
-        options = {};
-    }
 
-    // only execute on next event loop iteration
-    process.nextTick(function () {
-        try {
-            var res = schema_sync(documents, options);
-            callback(null, res);
-        } catch (e) {
-            callback(e);
-        }
-    }); 
-}
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+
+  // only execute on next event loop iteration
+  process.nextTick(function() {
+    try {
+      var res = schema_sync(documents, options);
+      callback(null, res);
+    } catch ( e ) {
+      callback(e);
+    }
+  });
+};
 
 module.exports.schema = schema;
 module.exports.schema_sync = schema_sync;

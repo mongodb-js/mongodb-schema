2d602360a5803bc2fda050e615ae30106061886f
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-05-03 14:12:04 +1000

    disable raw mode. print x, btype, comment only.

--- scripts/read_types.js
+++ scripts/read_types.js
@@ -1,22 +1,28 @@
 // script to read documents from the test.types collection created by `write_types.js`
+var BSON = require('bson').BSONPure.BSON;
 
 var MongoClient = require('mongodb').MongoClient,
     assert = require('assert');
 
 var url = 'mongodb://localhost:27017/test';
 
+
 MongoClient.connect(url, function(err, db) {
   assert.ifError(err);
 
   // Get the documents collection
-  var collection = db.collection('types');
+  var collection = db.collection('types', {raw: false});
 
   // find all documents and print them out
-  collection.find({}).toArray(function(err, docs) {
+  collection.find({}).sort({btype:1}).toArray(function(err, docs) {
     assert.ifError(err);
 
     // don't use JSON.stringify here, it loses a lot of information like _bsontype
-    console.log(docs);
+    docs.forEach(function (doc) {
+      console.log(doc.x, doc.btype, doc.comment);
+      console.log()
+      // console.log(BSON.deserialize(doc));
+    });
     db.close();
   });
 });

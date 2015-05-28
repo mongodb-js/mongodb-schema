8953f6a3b7e11e1897e53c126429c807de7d9d52
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-05-28 19:27:09 -0400

    fix(schema): fix embedded array probability

--- test/basic-embedded-array.test.js
+++ test/basic-embedded-array.test.js
@@ -1,9 +1,11 @@
 var getSchema = require('../');
 var assert = require('assert');
 var BSON = require('bson');
+var _ = require('lodash');
 
 describe('basic embedded array', function() {
   var following;
+  var following_ids;
   var docs = [
     {
       '_id': BSON.ObjectID('55581e0a9bf712d0c2b48d71'),
@@ -20,15 +22,29 @@ describe('basic embedded array', function() {
   ];
 
   before(function(done) {
-    following = getSchema('following', docs, done);
+    following = getSchema('following', docs, function() {
+      following_ids = following.fields.get('following_ids').fields.get('__basic__');
+      done();
+    });
+  });
+  it('should have 2 lengths for following_ids', function() {
+    assert.deepEqual(following.fields.get('following_ids').lengths, [1, 2]);
+  });
+
+  it('should have an average length of 1.5 for following_ids', function() {
+    assert.equal(following.fields.get('following_ids').average_length, 1.5);
+  });
+
+  it('should have a sum of probability for following_ids of 1', function() {
+    assert.equal(_.sum(following_ids.types.pluck('probability')), 1);
   });
 
   it('should have 33% String for following_ids', function() {
-    assert(following.fields.get('following_ids').fields.get('__basic__').types.get('String'), 1 / 3);
+    assert.equal(following_ids.types.get('String').probability, 1 / 3);
   });
 
   it('should have 66% ObjectID for following_ids', function() {
-    assert(following.fields.get('following_ids').fields.get('__basic__').types.get('ObjectID'), 2 / 3);
+    assert.equal(following_ids.types.get('ObjectID').probability, 2 / 3);
   });
 
   it('should serialize correctly', function() {

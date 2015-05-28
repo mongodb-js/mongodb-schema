d0f8cf20bf4b45d3a9123aaeb9beb874a68bbf45
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-05-28 18:39:59 -0400

    test for @rueckstiess to state what probabilities should be for embedded arrays

--- test/basic-embedded-array.test.js
+++ test/basic-embedded-array.test.js
@@ -11,13 +11,24 @@ describe('basic embedded array', function() {
     },
     {
       '_id': BSON.ObjectID('55582407aafa8fbbc57196e2'),
-      'following_ids': [BSON.ObjectID('55581e0a9bf712d0c2b48d71')]
+      'following_ids': [
+        BSON.ObjectID('55581e0a9bf712d0c2b48d71'),
+        '55581e0a9bf712d0c2b48d71'
+      ]
     },
 
   ];
 
   before(function(done) {
     following = getSchema('following', docs, done);
+  });
+
+  it('should have 33% String for following_ids', function() {
+    assert(following.fields.get('following_ids').fields.get('__basic__').types.get('String'), 1 / 3);
+  });
+
+  it('should have 66% ObjectID for following_ids', function() {
+    assert(following.fields.get('following_ids').fields.get('__basic__').types.get('ObjectID'), 2 / 3);
   });
 
   it('should serialize correctly', function() {

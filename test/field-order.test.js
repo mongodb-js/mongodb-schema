1b1ddf103edcca815738a1e6a0f92d1f58ed3cbc
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-06-24 10:56:17 +1000

    field sort order: _id on top, case-insensitive.

---
+++ test/field-order.test.js
@@ -0,0 +1,26 @@
+var getSchema = require('../');
+var assert = require('assert');
+var debug = require('debug')('mongodb-schema:test:field-order');
+
+describe('order of fields', function () {
+  it('should have _id fields always at top, even with uppercase fields', function(done) {
+    var docs = [ {FOO: 1, _id: 1, BAR: 1, zoo: 1} ];
+    getSchema('field.order', docs, function (err, schema) {
+      assert.ifError(err);
+      assert.deepEqual(schema.fields.map(function (field) {
+        return field.getId();
+      }), ['_id', 'BAR', 'FOO', 'zoo']);
+      done();
+    });
+  });
+  it('should sort in case-insensitive manner', function (done) {
+    var docs = [ {cb: 1, Ca: 1, cC: 1, a:1, b: 1} ];
+    getSchema('field.order', docs, function (err, schema) {
+      assert.ifError(err);
+      assert.deepEqual(schema.fields.map(function (field) {
+        return field.getId();
+      }), ['a', 'b', 'Ca', 'cb', 'cC']);
+      done();
+    });
+  });
+});

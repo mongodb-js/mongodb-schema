e3b40109d097d28abe3f8e596515d066d20195c9
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-06-25 18:26:24 +1000

    verify parent is passed down to values.

--- test/value-collection.test.js
+++ test/value-collection.test.js
@@ -1,10 +1,12 @@
 var ValueCollection = require('../lib/value-collection');
+var NumberType = require('../lib/type').Number;
 var _ = require('lodash');
 var assert = require('assert');
 var debug = require('debug')('mongodb-schema:test:value-collection');
 
 describe('ValueCollection', function () {
   var collection;
+
   before(function () {
     collection = new ValueCollection([
       {value: 0},
@@ -13,10 +15,19 @@ describe('ValueCollection', function () {
       {value: false},
       {value: []},
       {value: {}}
-    ], {parse: true});
-  })
+    ]);
+  });
+
   it('should accept any type of value', function () {
     assert.deepEqual(collection.serialize(), [0, '', null, false, [], {}]);
     assert.equal(collection.length, 6);
+  });
+
+  it('should pass collection\'s parent down to the values', function () {
+    var num = new NumberType(null);
+    num.values.reset([
+      {value: 250}
+    ]);
+    assert.equal(num.values.at(0).parent, num);
   });
 });

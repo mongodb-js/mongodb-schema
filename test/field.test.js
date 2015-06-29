0121f08e0947ae70ca52674184c0d56f46f6212e
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-06-29 15:24:25 +1000

    trigger types.length change event in field.

--- test/field.test.js
+++ test/field.test.js
@@ -30,4 +30,17 @@ describe('Field', function () {
   it('should return undefined for Field#type if no types present', function () {
     assert.equal(field.type, undefined);
   });
+
+  it('should trigger types.length events when adding a new type', function () {
+    field.types.addToType(15);
+    assert.equal(field.type, 'Number');
+    field.types.addToType("sfo");
+    assert.deepEqual(field.type, ['Number', 'String']);
+  });
+
+  it('should update .fields alias correctly', function () {
+    assert.equal(field.fields, null);
+    field.types.addToType({foo: 1});
+    assert.equal(field.fields, field.types.get('Document').fields);
+  });
 });

d0b88f83e98150d83bd6b558db857f2ec8645d43
Thomas Rueckstiess <thomas@rueckstiess.net>
2015-06-29 13:51:54 +1000

    unique and has_duplicates

--- test/basic-unique.test.js
+++ test/basic-unique.test.js
@@ -9,7 +9,12 @@ describe('unique', function() {
     },
     {
       _id: 2,
-      registered: true
+      registered: true,
+      code: null
+    },
+    {
+      _id: 3,
+      code: null
     }
   ];
 
@@ -24,13 +29,13 @@ describe('unique', function() {
     });
   });
 
-  it('should have count of 2 for `_id`', function() {
-    assert.equal(schema.fields.get('_id').count, 2);
+  it('should have count of 3 for `_id`', function() {
+    assert.equal(schema.fields.get('_id').count, 3);
   });
 
-  it('should have unique of 2 for `_id`', function() {
-    assert.equal(schema.fields.get('_id').unique, 2);
-    assert.equal(schema.fields.get('_id').types.get('Number').unique, 2);
+  it('should have unique of 3 for `_id`', function() {
+    assert.equal(schema.fields.get('_id').unique, 3);
+    assert.equal(schema.fields.get('_id').types.get('Number').unique, 3);
   });
 
   it('should not have duplicates for `_id`', function() {
@@ -41,8 +46,16 @@ describe('unique', function() {
     assert.equal(schema.fields.get('registered').count, 2);
   });
 
-  it('should have unique of 1 for `registered`', function() {
-    assert.equal(schema.fields.get('registered').unique, 1);
+  it('should have unique of 1 for `registered` type Boolean', function() {
+    assert.equal(schema.fields.get('registered').types.get('Boolean').unique, 1);
+  });
+
+  it('should have unique of 2 for `registered` overall', function() {
+    assert.equal(schema.fields.get('registered').unique, 2);
+  });
+
+  it('should have unique of 1 for `code`', function() {
+    assert.equal(schema.fields.get('code').types.get('Null').unique, 1);
   });
 
   it('should have duplicates for `registered`', function() {

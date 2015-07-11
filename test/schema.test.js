901a09aa823177f2a415f37b885e6150f8516b91
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-07-11 13:54:04 -0400

    fix(perf): don't emit so many unnecessary events

--- test/schema.test.js
+++ test/schema.test.js
@@ -1,13 +1,11 @@
-var Schema = require('../lib/schema');
-var schemaHelper = require('../lib');
+var schemaHelper = require('../');
+var Schema = schemaHelper.Schema;
 var assert = require('assert');
-var debug = require('debug')('mongodb-schema:test:schema');
 var es = require('event-stream');
 
-
-describe('Schema', function () {
+describe('Schema', function() {
   var schema;
-  beforeEach(function () {
+  beforeEach(function() {
     schema = new Schema();
   });
 
@@ -15,22 +13,32 @@ describe('Schema', function () {
     assert.ok(schema);
   });
 
-  it('should parse a simple document', function () {
-    schema.parse({foo: 1});
+  it('should parse a simple document', function() {
+    schema.parse({
+      foo: 1
+    });
     assert.ok(schema.fields.get('foo'));
     assert.equal(schema.count, 1);
   });
 
-  it('should parse a nested document', function () {
-    schema.parse({foo: {bar: 1}});
+  it('should parse a nested document', function() {
+    schema.parse({
+      foo: {
+        bar: 1
+      }
+    });
     assert.ok(schema.fields.get('foo'));
     assert.ok(schema.fields.get('foo').types.get('Document').fields.get('bar'));
     assert.equal(schema.count, 1);
     assert.equal(schema.fields.get('foo').types.get('Document').count, 1);
   });
 
-  it('should set up the parent tree all the way down', function () {
-    schema.parse({foo: {bar: [1, 2, 3]}});
+  it('should set up the parent tree all the way down', function() {
+    schema.parse({
+      foo: {
+        bar: [1, 2, 3]
+      }
+    });
     var foo = schema.fields.get('foo');
     assert.equal(foo.parent, schema);
     var subdoc = foo.types.get('Document');
@@ -45,95 +53,120 @@ describe('Schema', function () {
     assert.equal(val.parent, num);
   });
 
-  it('should trigger an `end` event at the end of parsing a stream', function (done) {
-    var docs = [{foo: 1}, {bar: 1, foo: 1}];
-    var src = es.readArray(docs);
-    var stream = src.pipe(schema.stream());
-    stream.on('end', function () {
-      assert.equal(schema.count, 2);
-      done();
-    });
+  it('should trigger an `end` event at the end of parsing a stream', function(done) {
+    var docs = [{
+      foo: 1
+    }, {
+      bar: 1,
+      foo: 1
+    }];
+    es.readArray(docs)
+      .pipe(schema.stream())
+      .pipe(es.wait(function() {
+        assert.equal(schema.count, 2);
+        done();
+      }));
   });
 
-  it('should trigger `data` events for each doc', function (done) {
-    var docs = [{foo: 1}, {bar: 1, foo: 2}];
+  it('should trigger `data` events for each doc', function(done) {
+    var docs = [{
+      foo: 1
+    }, {
+      bar: 1,
+      foo: 2
+    }];
     var src = es.readArray(docs);
     var count = 0;
     src.pipe(schema.stream())
-      .on('data', function (doc) {
-        count ++;
-      })
-      .on('end', function () {
+      .pipe(es.map(function(doc, cb) {
+        count++;
+        cb(null, doc);
+      }))
+      .pipe(es.wait(function() {
         assert.equal(count, 2);
         done();
-      });
+      }));
   });
-
 });
 
 describe('Schema Helper', function() {
-  it('should be able to handle an array as input', function (done) {
-    var docs = [{foo: 1}, {bar: 1, foo: 2}];
+  it('should be able to handle an array as input', function(done) {
+    var docs = [{
+      foo: 1
+    }, {
+      bar: 1,
+      foo: 2
+    }];
     var src = es.readArray(docs);
-    var schema;
-    schema = schemaHelper('with.stream', src, function () {
+    var schema = schemaHelper('with.stream', src, function() {
       assert.ok(schema.fields.get('foo'));
       assert.ok(schema.fields.get('bar'));
       done();
     });
   });
 
-  it('should be able to handle a stream as input', function (done) {
-    var docs = [{foo: 1}, {bar: 1, foo: 2}];
-    var schema;
-    schema = schemaHelper('with.stream', docs, function () {
+  it('should be able to handle a stream as input', function(done) {
+    var docs = [{
+      foo: 1
+    }, {
+      bar: 1,
+      foo: 2
+    }];
+    var schema = schemaHelper('with.stream', docs, function() {
       assert.ok(schema.fields.get('foo'));
       assert.ok(schema.fields.get('bar'));
       done();
     });
   });
 
-  it('should be able to handle an object as input that exposes a .stream() method', function (done) {
-    var docs = [{foo: 1}, {bar: 1, foo: 2}];
+  it('should be able to handle an object as input that exposes a .stream() method', function(done) {
+    var docs = [{
+      foo: 1
+    }, {
+      bar: 1,
+      foo: 2
+    }];
     var src = es.readArray(docs);
-    var obj = {name: 'Container Object', stream: function () { return src; }};
-    var schema;
-    schema = schemaHelper('with.stream', obj, function () {
+    var obj = {
+      name: 'Container Object',
+      stream: function() {
+        return src;
+      }
+    };
+    var schema = schemaHelper('with.stream', obj, function() {
       assert.ok(schema.fields.get('foo'));
       assert.ok(schema.fields.get('bar'));
       done();
     });
   });
 
-  it('schema object should also trigger `end` event when done parsing', function (done) {
-    var docs = [{foo: 1}, {bar: 1, foo: 2}];
-    var src = es.readArray(docs);
-    var schema;
-    schema = schemaHelper('with.stream', src, function () {
-      assert.ok(schema.fields.get('foo'));
-      assert.ok(schema.fields.get('bar'));
-    }).on('end', function(schema) {
-      assert.equal(schema.count, 2);
-      done();
-    });
-  });
+  it('schema object should also trigger `data` events for each doc', function(done) {
+    var docs = [{
+      foo: 1
+    }, {
+      bar: 1,
+      foo: 2
+    }];
 
-  it('schema object should also trigger `data` events for each doc', function (done) {
-    var docs = [{foo: 1}, {bar: 1, foo: 2}];
-    var src = es.readArray(docs);
     var count = 0;
-    var schema;
-    schema = schemaHelper('with.stream', src, function () {
-      assert.ok(schema.fields.get('foo'));
-      assert.ok(schema.fields.get('bar'));
-    }).on('data', function(doc, innerSchema) {
-      count ++;
-      assert.ok(doc);
-      assert.equal(schema, innerSchema);
-    }).on('end', function () {
-      assert.equal(count, 2);
-      done();
+    var schema = new Schema({
+      ns: 'with.stream'
     });
+    es.readArray(docs)
+      .pipe(schema.stream())
+      .pipe(es.map(function(doc, cb) {
+        count++;
+        assert.ok(schema.fields.get('foo'));
+        if (count === 2) {
+          assert.ok(schema.fields.get('bar'));
+        } else {
+          assert.equal(schema.fields.get('bar'), undefined);
+        }
+        cb(null, doc);
+      }))
+      .pipe(es.wait(function() {
+        assert.equal(count, 2);
+        done();
+      }));
   });
-
 });

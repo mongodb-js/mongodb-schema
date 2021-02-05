2cf958e6843817d28d4032c1d5691e8dc12a78e4
Alex Bjørlig <alexbjorlig@icloud.com>
2021-02-05 13:39:17 +0100

    Added linting check in package.json

--- test/promise-or-callback.test.js
+++ test/promise-or-callback.test.js
@@ -1,61 +1,64 @@
 var getSchema = require('../');
 var assert = require('assert');
-var _ = require("lodash");
+var _ = require('lodash');
 
 describe('getSchema should return promise', function() {
-    var docs = [
-        { foo: 'bar'},
-        { country: 'Croatia'},
-        { country: 'Croatia'},
-        { country: 'England'},
-    ]
+  var docs = [
+    { foo: 'bar'},
+    { country: 'Croatia'},
+    { country: 'Croatia'},
+    { country: 'England'},
+  ];
 
-    it('Check if return value is a promise', () => {
-        var result = getSchema(docs);
-        assert.strictEqual(result instanceof Promise, true);
+  it('Check if return value is a promise', () => {
+    var result = getSchema(docs);
+    assert.strictEqual(result instanceof Promise, true);
+  });
+
+  it('Check that promise returns expected schema', async() => {
+    const result = await getSchema(docs);
+    const fieldNames = result.fields.map(v => v.name);
+    assert.deepStrictEqual(fieldNames, ['country', 'foo']);
+  });
+
+  describe('Check callback and promise return the same thing; success', () => {
+    let promiseResponse;
+    let callbackResponse;
+
+    before((done) => {
+      getSchema(docs).then((result) => {
+        promiseResponse = result;
+      });
+      getSchema(docs, (err, callbackResult) => {
+        if (err) {
+          throw err;
+        }
+        callbackResponse = callbackResult;
+        done();
+      });
     });
 
-    it('Check that promise returns expected schema', async () => {
-        const result = await getSchema(docs);
-        const fieldNames = result.fields.map(v => v.name);
-        assert.deepStrictEqual(fieldNames, ['country', 'foo']);
-    })
+    it('Using callback and promise should return the same thing', () => {
+      assert(_.isEqual(promiseResponse, callbackResponse));
+    });
+  });
 
-    describe('Check callback and promise return the same thing; success', () => {
+  describe('Check callback and promise return the same thing; failure', () => {
+    let promiseErrMessage;
+    let callbackErrMessage;
 
-        let promiseResponse;
-        let callbackResponse;
-
-        before((done) => {
-            getSchema(docs).then((result) => promiseResponse = result);
-            getSchema(docs, (err, callbackResult) => {
-                callbackResponse = callbackResult;
-                done();
-            })
+    before((done) => {
+      getSchema({foo: 'bar'}, (err) => {
+        getSchema({foo: 'bar'}).catch(error => {
+          promiseErrMessage = error.message;
         });
-
-        it('Using callback and promise should return the same thing', () => {
-            assert(_.isEqual(promiseResponse, callbackResponse))
-        });
+        callbackErrMessage = err.message;
+        done();
+      });
     });
 
-    describe('Check callback and promise return the same thing; failure', () => {
-
-        let promiseErrMessage;
-        let callbackErrMessage;
-
-        before((done) => {
-            
-            getSchema({foo: 'bar'}, (err) => {
-                getSchema({foo: 'bar'}).catch(err => promiseErrMessage = err.message)
-                callbackErrMessage = err.message;
-                done();
-            })
-        });
-
-        it('Using callback and promise should give same err message', () => {
-            assert.strictEqual(promiseErrMessage, callbackErrMessage);
-        });
+    it('Using callback and promise should give same err message', () => {
+      assert.strictEqual(promiseErrMessage, callbackErrMessage);
     });
-
+  });
 });

262ffb81e9d50f78e42767038e1c1e4b16356a60
Rhys <Anemy@users.noreply.github.com>
2025-02-26 13:02:56 -0500

    chore(test): add integration test for mongodb schema rule generation COMPASS-8890 (#232)

--- test/integration/generateAndValidate.test.ts
+++ test/integration/generateAndValidate.test.ts
@@ -2,6 +2,8 @@ import { analyzeDocuments } from '../../src';
 import Ajv2020 from 'ajv/dist/2020';
 import assert from 'assert';
 import { ObjectId, Int32, Double, EJSON } from 'bson';
+import { MongoClient, type Db } from 'mongodb';
+import { mochaTestServer } from '@mongodb-js/compass-test-server';
 
 const bsonDocuments = [{
   _id: new ObjectId('67863e82fb817085a6b0ebad'),
@@ -41,6 +43,78 @@ describe('Documents -> Generate schema -> Validate Documents against the schema'
       const valid = validate(relaxedEJSONDoc);
       if (validate.errors) console.error('Validation failed', validate.errors);
       assert.strictEqual(valid, true);
+    }
+  });
+});
+
+describe('Documents -> Generate schema -> Use schema in validation rule in MongoDB -> Validate documents against the schema', function() {
+  let client: MongoClient;
+  let db: Db;
+  const cluster = mochaTestServer();
+
+  before(async function() {
+    // Create the schema validation rule.
+    const analyzedDocuments = await analyzeDocuments(bsonDocuments);
+    const schema = await analyzedDocuments.getMongoDBJsonSchema();
+    const validationRule = {
+      $jsonSchema: schema
+    };
+
+    // Connect to the mongodb instance.
+    const connectionString = cluster().connectionString;
+    client = new MongoClient(connectionString);
+    await client.connect();
+    db = client.db('test');
+
+    // Create a collection with the schema validation in Compass.
+    await db.createCollection('books', {
+      validator: validationRule
+    });
+  });
+  after(async function() {
+    await client?.close();
+  });
+
+  it('allows inserting valid documents', async function() {
+    await db.collection('books').insertMany(bsonDocuments);
+  });
+
+  it('prevents inserting invalid documents', async function() {
+    const invalidDocs = [{
+      _id: new ObjectId('67863e82fb817085a6b0ebba'),
+      title: 'Pineapple 1',
+      year: new Int32(1983),
+      genres: [
+        'crimi',
+        'comedy',
+        {
+          short: 'scifi',
+          long: 'science fiction'
+        }
+      ],
+      number: 'an invalid string'
+    }, {
+      _id: new ObjectId('67863eacfb817085a6b0ebbb'),
+      title: 'Pineapple 2',
+      year: 'year a string'
+    }, {
+      _id: new ObjectId('67863eacfb817085a6b0ebbc'),
+      title: 123,
+      year: new Int32('1999')
+    }, {
+      _id: new ObjectId('67863eacfb817085a6b0ebbc'),
+      title: 'No year'
+    }];
+
+    for (const doc of invalidDocs) {
+      try {
+        await db.collection('books').insertOne(doc);
+
+        throw new Error('This should not be reached');
+      } catch (e: any) {
+        const expectedMessage = 'Document failed validation';
+        assert.ok(e.message.includes(expectedMessage), `Expected error ${e.message} message to include "${expectedMessage}", doc: ${doc._id}`);
+      }
     }
   });
 });
